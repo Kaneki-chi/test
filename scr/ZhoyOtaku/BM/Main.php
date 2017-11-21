e3b0c053f9239e7ee8605508b6dda0dd6ae32b2d
ZhoyOtaku <30436794+Kaneki-chi@users.noreply.github.com>
2017-11-21 17:10:54 -0500

    Update Main.php

--- scr/ZhoyOtaku/BM/Main.php
+++ scr/ZhoyOtaku/BM/Main.php
@@ -1,1 +1,16 @@
-namespace BanManager
+namespace KitsOp
+
+use pocketmine\plgin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Command;
+use pocketmine\CommandSender;
+use ZhoyOtaku\KitsOp\Kits;
+use pocketmine\Player;
+use pocketmine\player\PlayerJoinEvent;
+use pocketmine\player\PlayerQuitEvent;
+use pocketmine\utils\Config;
+use pocketmine\utils\TexFormat as C;
+use pocketmine\command\CommandExecutor;
+use pocketmine\tile\Sing;
+
+Class Main extends PluginBase{
